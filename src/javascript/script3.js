b6c774a9f529d58c2b1ecbbb26d2d5a3d9dd3163
bibsveloso <beatrizoslov6@gmail.com>
2024-11-09 09:12:56 -0300

    space

--- src/javascript/script3.js
+++ src/javascript/script3.js
@@ -1,11 +1,18 @@
 document.getElementById("search").addEventListener("input", function() {
     const searchTerm = this.value.toLowerCase();
+    const searchWords = searchTerm.split(" ").filter(word => word); // Divide o termo de pesquisa em palavras e remove espaços em branco
     const items = document.querySelectorAll(".items .item, .items .card");
     let hasResults = false;
 
+    // Função para remover acentos
+    const normalizeText = text => text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
     items.forEach(item => {
-        const itemText = item.textContent.toLowerCase();
-        if (itemText.includes(searchTerm)) {
+        const itemText = normalizeText(item.textContent.toLowerCase());
+        // Verifica se todas as palavras de busca estão no texto do item
+        const match = searchWords.every(word => itemText.includes(normalizeText(word)));
+        
+        if (match) {
             item.style.display = "block";
             hasResults = true;
         } else {
